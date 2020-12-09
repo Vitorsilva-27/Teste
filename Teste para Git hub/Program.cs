27aa81e7008f4af9209d80a815e3a2ce037629ea
Vitor Silva <vitor_crsilva97@hotmail.com>
2020-12-09 10:30:59 -0300

    Teste 2

--- Teste para Git hub/Program.cs
+++ Teste para Git hub/Program.cs
@@ -12,6 +12,7 @@ namespace Teste_para_Git_hub
         {
             Console.WriteLine("Bom dia !");
             Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite");
         }
     }
 }
